cfd5078db934f3f27639434ea0d7193e6a0f2cfb
tariqkjm7 <d.alatoum@gmail.com>
2021-08-01 17:10:31 +0300

    adding html file

---
+++ app.js
@@ -0,0 +1,25 @@
+var Name = prompt("what is your name")
+alert("welcome to my website "+Name)
+console.log("welcome to my website "+Name)
+
+let age = prompt("how old are you ?")
+
+if(age > 24){
+alert("I'am 24 years old you are older than me ")}
+
+else if(age == 24){ alert("I'am 24 years old we are at the same age")}
+else {alert("I'am 24 years old you are younger than me ")}
+
+console.log("I'am "+age+" years old")
+
+let major = prompt("what is your major")
+alert("I studied as a  "+major)
+console.log("I studied as a  "+major)
+
+
+
+
+let Favnum = prompt("what is favorite number ")
+alert("My favorite number is "+ Favnum)
+console.log("My favorite number is "+ Favnum)
+
